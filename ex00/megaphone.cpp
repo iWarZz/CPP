ec9b27f9cd92f0736de438644fb51c79576e775b
iWarZz <gratueur678@gmail.com>
2023-12-01 14:14:28 +0100

    ok

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,18 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ssalor <ssalor@student.42.fr>              +#+  +:+       +#+        */
+/*   By: warzz <warzz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 14:10:06 by warzz             #+#    #+#             */
-/*   Updated: 2023/11/28 14:48:50 by ssalor           ###   ########.fr       */
+/*   Updated: 2023/12/01 14:13:34 by warzz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
 int	main(int ac, char **av)
 {
-	if (ac == 2)
+	if (ac > 1)
 	{
-		
+		for (int i = 1; av[i]; i++)
+		{
+			for (int j = 0; av[i][j]; j++)
+				std::cout << (char)toupper(av[i][j]);
+			if (i < ac - 1)
+				std::cout << ' ';
+		}
 	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
 	return (1);
 }
