933f3473fed56c6f317d970e35dcded7c30a9152
Steven Salor <ssalor@k1r3p4.42mulhouse.fr>
2024-03-12 15:13:58 +0100

    cpp03ClapTrap.cpp

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -6,7 +6,48 @@
 /*   By: ssalor <ssalor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 13:47:03 by ssalor            #+#    #+#             */
-/*   Updated: 2024/03/08 13:47:05 by ssalor           ###   ########.fr       */
+/*   Updated: 2024/03/12 15:13:15 by ssalor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "ClapTrap.hpp"
+
+void ClapTrap::attack(const std::string& target)
+{
+	if (this->energy_points == 0)
+	{
+		std::cout << "ClapTrap is out of energy" << std::endl;
+		return ;
+	}
+	std::cout << "ClapTrap " << this->name << " attacks "
+			  << target << " causing <amount> "
+			  << "points of damage!" << std::endl;
+	this->energy_points -= 1;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (amount >= this->hit_points || this->hit_points == 0)
+	{
+		std::cout << "ClapTrap " << this->name << " died" << cout::endl;
+		return ;
+	}
+	this->hit_points -= amount;
+	std::cout << "ClapTrap " << this->name << " took "
+			  << amount << " points of damage" << cout::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->energy_points == 0)
+	{
+		std::cout << "ClapTrap is out of energy points!" << std::endl;
+		return ;
+	}
+	this->hit_points += amount;
+	this->energy_points -= 1;
+	std::cout << "ClapTrap " << this->name 
+			  << " has been repaired and gained " << amount
+			  << " hit points" << cout::endl;
+}
--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -6,13 +6,26 @@
 /*   By: ssalor <ssalor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 13:47:07 by ssalor            #+#    #+#             */
-/*   Updated: 2024/03/08 13:50:48 by ssalor           ###   ########.fr       */
+/*   Updated: 2024/03/12 14:32:01 by ssalor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP
 # define CLAPTRAP_HPP
 
+# include <string>
 
+class ClapTrap
+{
+    private:
+		std::string		name;
+		unsigned int	hit_points;
+		unsigned int	energy_points;
+		unsigned int	attack_damage;
+    public:
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+}
 
 #endif
