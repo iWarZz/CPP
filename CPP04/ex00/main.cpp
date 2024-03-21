313f1b27eee4b809c868f229713ab78795d894aa
Steven Salor <ssalor@k1r4p1.42mulhouse.fr>
2024-03-21 15:45:58 +0100

    inprogress

---
+++ CPP04/ex00/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssalor <ssalor@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/21 15:13:52 by ssalor            #+#    #+#             */
+/*   Updated: 2024/03/21 15:42:15 by ssalor           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include ""
+
+int main()
+{
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound(); //will output the cat sound!
+    j->makeSound();
+    meta->makeSound();
+
+    return 0;
+}
