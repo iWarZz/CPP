dfcfab00ddf11b83b221fa3167839b351c9725a9
Steven Salor <ssalor@k1r4p16.42mulhouse.fr>
2024-08-22 07:04:38 +0200

    oui

--- CPP05/ex00/Bureaucrat.cpp
+++ CPP05/ex00/Bureaucrat.cpp
@@ -6,7 +6,57 @@
 /*   By: ssalor <ssalor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 12:26:10 by ssalor            #+#    #+#             */
-/*   Updated: 2024/07/10 12:26:10 by ssalor           ###   ########.fr       */
+/*   Updated: 2024/08/22 07:04:19 by ssalor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(void) name("Noname")
+{
+    this->grade = 150;
+    std::cout << "Bureaucrat new object named : " << this->name << " with " << this->grade << " grade." std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade) name(name)
+{
+    if (grade < 1)
+        throw Bureaucrat::GradeTooLowException();
+    else if (grade > 150)
+        throw Bureaucrat::GradeTooHighException();
+    this->grade = grade;
+    std::cout << "Bureaucrat new object named : " << this->name << " with " << this->grade << " grade." std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &bureaucrat) name(bureaucrat.getName())
+{
+    *this = bureaucrat;
+    std::cout << "Bureaucrat new copied object named : " << this->name << std::endl;
+}
+
+Bureaucrat::~Bureaucrat(void)
+{
+    std::cout << "Bureaucrat delete object named : " << this->name << std::endl;
+}
+
+
+
+const   std::string Bureaucrat::getName(void) const
+{
+    return (this->name);
+}
+
+int     Bureaucrat::getGrade(void) const
+{
+    return (this->grade);
+}
+
+void    incrementGrade(void)
+{
+    
+}
+
+void    decrementGrade(void)
+{
+    
+}
--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -6,21 +6,45 @@
 /*   By: ssalor <ssalor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 12:18:33 by ssalor            #+#    #+#             */
-/*   Updated: 2024/07/10 12:22:49 by ssalor           ###   ########.fr       */
+/*   Updated: 2024/08/22 07:03:09 by ssalor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
 #define BUREAUCRAT_HPP
 
+#include <iostream>
 
 class Bureaucrat
 {
 	private:
 		const std::string	name;
-		size_t				grade;
+		int					grade;
+		
 	public:
+		Bureaucrat(void);
+		Bureaucrat(const std::string &name, int grade);
+		Bureaucrat(const Bureaucrat &bureaucrat);
+		~Bureaucrat(void);
+		
+		const std::string	&getName(void) const;
+		int					getGrade(void) const;
+
+		void 				incrementGrade(void);
+		void 				decrementGrade(void);
 	
+	class GradeTooHighException; public std::exeption
+	{
+		public:
+			virtual const char* what() const throw();
+	};
+	
+	
+	class GradeTooLowException; public std::exeption
+	{
+		public:
+			virtual const char* what() const throw();
+	};
 };
 
 #endif
